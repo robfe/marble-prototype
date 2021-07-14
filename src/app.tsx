a19bd9c3e3f83697c171c46f9cac8d1b630ef6cf
robfe <robert.ensor@gmail.com>
2021-07-15 11:04:07 +1200

    Got routing going on the default mobx tsx template

--- src/app.tsx
+++ src/app.tsx
@@ -2,36 +2,12 @@ import * as React from "react";
 import "./styles.css";
 import { Store } from "./store";
 import * as mobxReact from "mobx-react";
+import Routes from "Views/Nav/Routes";
 
 const store = new Store<string>();
 
 export default mobxReact.observer(function App() {
   const [value, setValue] = React.useState("");
 
-  return (
-    <div className="App">
-      <h1>MobX React Typescript Starter</h1>
-      <h2>Add a section:</h2>
-      <input
-        value={value}
-        onChange={(e) => {
-          setValue(e.currentTarget.value);
-        }}
-      />
-      <button
-        disabled={value === ""}
-        onClick={() => {
-          store.addSection(value);
-        }}
-      >
-        Add section
-      </button>
-      <h2>Sections:</h2>
-      <ul>
-        {store.sections.map((section) => (
-          <li>{section}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <Routes />;
 });
