12945ef551ccc97a84428183fd315ed2b22aea89
robfe <robert.ensor@gmail.com>
2021-07-15 11:07:41 +1200

    Imported and tested ReactStrap

--- src/app.tsx
+++ src/app.tsx
@@ -3,11 +3,17 @@ import "./styles.css";
 import { Store } from "./store";
 import * as mobxReact from "mobx-react";
 import Routes from "Views/Nav/Routes";
+import { Button } from "reactstrap";
 
 const store = new Store<string>();
 
 export default mobxReact.observer(function App() {
   const [value, setValue] = React.useState("");
 
-  return <Routes />;
+  return (
+    <>
+      <Routes />
+      <Button color="danger">Danger!</Button>
+    </>
+  );
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { render } from "react-dom";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import App from "./app";
 
 const rootElement = document.getElementById("root");
