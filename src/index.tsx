a519e97d439e8e5bcb17c9c2f38024dda90ceb82
robfe <robert.ensor@gmail.com>
2021-07-15 13:55:17 +1200

    Enabled bootstrap theming

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { render } from "react-dom";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/custom-btsp.scss";
 
 import App from "./app";
 
